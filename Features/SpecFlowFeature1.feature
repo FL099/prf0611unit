f213166ef4bea31856f0831663f3747d2895bb5c
Florian <FL099@users.noreply.github.com>
2020-11-06 10:42:57 +0100

    final commit

---
+++ Features/SpecFlowFeature1.feature
@@ -0,0 +1,22 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be able to convert to different number formats
+
+@mytag
+Scenario: convert hexa
+	Given the input is <affe>
+	When the input is converted from 16
+	Then the result should be 45054
+
+@mytag
+Scenario: convert octa
+	Given the input is <11147>
+	When the input is converted from 8
+	Then the result should be 4711
+
+@mytag
+Scenario: convert biny
+	Given the input is <1100>
+	When the input is converted from 2
+	Then the result should be 12
---
+++ Features/SpecFlowFeature1Steps.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace XUnitTestProject1.Features
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        private string inp = "";
+        private int outp;
+
+        [Given(@"the input is (.*)")]
+        public void GivenTheInputIs(string p0)
+        {
+            inp = p0;
+        }
+
+        [When(@"the input is converted from (.*)")]
+        public void WhenTheInputIsConvertedFrom(int p0)
+        {
+            outp = System.Convert.ToInt32(inp, p0);
+        }
+
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            Assert.Equals(p0, outp);
+        }
+    }
+}
